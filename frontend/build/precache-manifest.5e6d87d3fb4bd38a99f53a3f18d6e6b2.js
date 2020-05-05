6d06704be48187f2612f6aefb6babdd7db0b7008
riley <riley0422@icloud.com>
2020-05-05 23:14:42 +0900

    frontend

---
+++ frontend/build/precache-manifest.5e6d87d3fb4bd38a99f53a3f18d6e6b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db0a8bfe86ad36c779722ea5c200426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "487d028d78699f731a05",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8cf18a1ded2c320f903e",
+    "url": "/static/js/2.b5db9a41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b5db9a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487d028d78699f731a05",
+    "url": "/static/js/main.421b3336.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
